293eab630be2a3161f553aa3ab0ddafc9079771a
PixelArtist <59205532+truonghocyeuquy@users.noreply.github.com>
2022-12-27 23:44:29 +0700

    Adding Activity Indicator

---
+++ MAUIsland/Features/Gallery/ActivityIndicator/Pages/ActivityIndicatorPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIsland;
+
+public partial class ActivityIndicatorPageViewModel : NavigationAwareBaseViewModel
+{
+    #region [CTor]
+    public ActivityIndicatorPageViewModel(IAppNavigator appNavigator)
+                                                : base(appNavigator)
+    {
+
+    }
+    #endregion
+
+    #region [Fields]
+
+    #endregion
+}
--- MAUIsland/MauiProgram.cs
+++ MAUIsland/MauiProgram.cs
@@ -58,6 +58,7 @@ public static class MauiProgram
         builder.Services.AddPage<ButtonPage, ButtonPageViewModel>();
         builder.Services.AddPage<EditorPage, EditorPageViewModel>();
         builder.Services.AddPage<MenuBarPage, MenuBarPageViewModel>();
+        builder.Services.AddPage<ActivityIndicatorPage, ActivityIndicatorPageViewModel>();
         return builder;
     }
 
