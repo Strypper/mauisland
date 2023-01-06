389c62c426847c2ebb380bad3d7924889b837609
Nghia Duong Tien <nghia.duong@idealogic.com.vn>
2023-01-06 16:43:54 +0700

    first commit

--- MAUIsland/Features/Gallery/MAUI/RaditoButton/Pages/RadioButtonPage.xaml.cs
+++ MAUIsland/Features/Gallery/MAUI/RaditoButton/Pages/RadioButtonPage.xaml.cs
@@ -1,13 +1,29 @@
+using System.Diagnostics;
+
 namespace MAUIsland;
 
 public partial class RadioButtonPage
 {
     #region [CTor]
-    public RadioButtonPage()
+    public RadioButtonPage(RadioButtonPageViewModel vm)
     {
         InitializeComponent();
+        BindingContext= vm;
     }
+
     #endregion
 
-
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        example1.IsVisible = !example1.IsVisible;
+        code1.IsVisible= !code1.IsVisible;
+        if(example1.IsVisible)
+        {
+            showCode1.Text = "Show Code";
+        }
+        else
+        {
+            showCode1.Text = "Hide Code";
+        }
+    }
 }
--- MAUIsland/Features/Gallery/MAUI/RaditoButton/Pages/RadioButtonPageViewModel.cs
+++ MAUIsland/Features/Gallery/MAUI/RaditoButton/Pages/RadioButtonPageViewModel.cs
@@ -10,4 +10,10 @@ public partial class RadioButtonPageViewModel : NavigationAwareBaseViewModel
     }
     #endregion
 
+    #region [Properties]
+
+    [ObservableProperty]
+    string codeExample1 = "<VerticalStackLayout>\r\n    <RadioButton Content=\"Cat\"\r\n         IsChecked=\"True\"/>\r\n    <RadioButton Content=\"Dog\"/>\r\n    <RadioButton Content=\"Fish/>\r\n    <RadioButton Content=\"Bird\"/>\r\n</VerticalStackLayout>";
+    #endregion
+
 }
