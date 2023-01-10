c12e94fd544690397df49ec01ec24572bb16c7b3
Xerath <abcxyz01233210@gmail.com>
2023-01-11 00:17:24 +0700

    Add CheckBox with Label

--- MAUIsland/Features/Gallery/MAUI/CheckBox/Pages/CheckBoxPageViewModel.cs
+++ MAUIsland/Features/Gallery/MAUI/CheckBox/Pages/CheckBoxPageViewModel.cs
@@ -6,7 +6,8 @@ public partial class CheckBoxPageViewModel : NavigationAwareBaseViewModel
     public CheckBoxPageViewModel(IAppNavigator appNavigator)
                                     : base(appNavigator)
     {
-
+        OnCyanButtonClicked = new Command(() => currentColor = "#28C2D1");
+        OnYellowButtonClicked = new Command(() => currentColor = "#F7B548");
     }
     #endregion
 
@@ -15,6 +16,25 @@ public partial class CheckBoxPageViewModel : NavigationAwareBaseViewModel
     bool isChecked;
 
     [ObservableProperty]
-    string standardCheckBoxXamlCode = "";
+    string currentColor = "F2F1F1";
+
+    [ObservableProperty]
+    string standardCheckBoxXamlCode = "<CheckBox />";
+
+    [ObservableProperty]
+    string checkBoxWithColorXamlCode = "<CheckBox Color=\"#FFFFFF\"/>";
+
+    [ObservableProperty]
+    string checkBoxTrueByDefaultXamlCode = "<CheckBox IsChecked=\"True\"/>";
+
+    [ObservableProperty]
+    string checkBoxWithBindingXamlCode = "<CheckBox IsChecked=\"{x:Binding IsChecked, Mode=TwoWay}\"\r\nColor=\"{x:Binding CurrentColor, Mode=OneWay}\"/>";
+
+    [ObservableProperty]
+    string checkBoxWithLabelXamlCode = "Label Text=\"CheckBox\"/>\r\n<CheckBox/>";
+
+    public ICommand OnCyanButtonClicked { get; private set; }
+
+    public ICommand OnYellowButtonClicked { get; private set; }
     #endregion
 }
