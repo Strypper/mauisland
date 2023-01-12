04a95f171d2bcd8c2759d2dd111307d283ac3403
Xerath <abcxyz01233210@gmail.com>
2023-01-12 22:29:58 +0700

    Add Chat Hub

---
+++ MAUIsland/Features/Chat/Pages/ChatPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUIsland.Features.Chat.Pages;
+
+public partial class ChatPage : ContentPage
+{
+	public ChatPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIsland/Features/Chat/Pages/ChatPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIsland.Features.Chat.Pages
+{
+    internal class ChatPageViewModel
+    {
+    }
+}
