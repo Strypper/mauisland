4668b5fac6c3843d7b197ea71fc1d1cc7f175d96
unknown <ocgrb.strypperjason115@gmail.com>
2023-01-13 23:21:51 +0700

    Refactor Service Helper

--- MAUIsland/Features/Chat/Pages/ChatPage.xaml.cs
+++ MAUIsland/Features/Chat/Pages/ChatPage.xaml.cs
@@ -18,7 +18,7 @@ public partial class ChatPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        var route = AppRoutes.ButtonPage;
+        var route = AppRoutes.ChatPage;
 
         var viewName = route[0].ToString().ToUpper() + route.Substring(1, route.Length - 1);
 
@@ -26,32 +26,8 @@ public partial class ChatPage
         var pageType = Type.GetType(pageFullName);
         var page = ServiceHelper.GetService<Page>(pageType);
 
-        //var viewModelFullName = $"MAUIsland.{viewName}ViewModel";
-        //var viewModelType = Type.GetType(viewModelFullName);
-        //var viewModel = ServiceHelper.GetService(viewModelType);
-
         Window secondWindow = new(page);
         Application.Current.OpenWindow(secondWindow);
     }
 
-}
-static class ServiceHelper
-{
-    public static TService GetService<TService>() => Current.GetService<TService>();
-    public static TService GetService<TService>(Type type) where TService : class
-        => GetService(type) as TService;
-
-    public static object GetService(Type type) => Current.GetService(type);
-
-    public static IServiceProvider Current
-    {
-        get
-        {
-#if WINDOWS
-            return MauiWinUIApplication.Current.Services;
-#endif
-
-            return null;
-        }
-    }
 }
