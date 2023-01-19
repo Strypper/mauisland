618a5858ec1e17ddbd5b3135989d7562fc28c401
Aspodel D <dattran2309@gmail.com>
2023-01-19 17:52:11 +0700

    add basic example for collectionview

--- src/Features/Gallery/Pages/BuiltIn/CollectionView/CollectionViewPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/CollectionView/CollectionViewPageViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace MAUIsland;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Contacts;
+
+namespace MAUIsland;
 
 
 public partial class CollectionViewPageViewModel : NavigationAwareBaseViewModel
@@ -10,4 +14,61 @@ public partial class CollectionViewPageViewModel : NavigationAwareBaseViewModel
 
     }
     #endregion
+
+    static List<Monkey> monkeyList = new List<Monkey>()
+    {
+        new Monkey()
+        {
+            Name = "Baboon",
+            Location = "Africa & Asia",
+            Details = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+        },
+        new Monkey()
+        {
+            Name = "Capuchin Monkey",
+            Location = "Central & South America",
+            Details = "The capuchin monkeys are New World monkeys of the subfamily Cebinae. Prior to 2011, the subfamily contained only a single genus, Cebus.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+        },
+        new Monkey()
+        {
+            Name = "Blue Monkey",
+            Location = "Central and East Africa",
+            Details = "The blue monkey or diademed monkey is a species of Old World monkey native to Central and East Africa, ranging from the upper Congo River basin east to the East African Rift and south to northern Angola and Zambia",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+        },
+        new Monkey()
+        {
+            Name = "Squirrel Monkey",
+            Location = "Central & South America",
+            Details = "The squirrel monkeys are the New World monkeys of the genus Saimiri. They are the only genus in the subfamily Saimirinae. The name of the genus Saimiri is of Tupi origin, and was also used as an English name by early researchers.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg"
+        },
+        new Monkey()
+        {
+            Name = "Golden Lion Tamarin",
+            Location = "Brazil",
+            Details = "The golden lion tamarin also known as the golden marmoset, is a small New World monkey of the family Callitrichidae.",
+            ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg"
+        }
+    };
+
+
+
+    #region [Properties]
+    [ObservableProperty]
+    IEnumerable<Monkey> monkeys = new List<Monkey>(monkeyList);
+    #endregion
+
+}
+
+
+public class Monkey
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsFavorite { get; set; }
 }
