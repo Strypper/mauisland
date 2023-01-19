15710d728a9200b472b006ae022e529ddfc326f1
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2023-01-19 23:43:48 +0700

    Add Swipe View

--- src/Features/Gallery/Pages/BuiltIn/SwipeView/SwipeViewPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/SwipeView/SwipeViewPageViewModel.cs
@@ -14,6 +14,8 @@ public partial class SwipeViewPageViewModel : NavigationAwareBaseViewModel
     #region [Properties]
     [ObservableProperty]
     IControlInfo controlInformation;
+    [ObservableProperty]
+    string standardSwipeViewXamlCode = "<SwipeView>\r\n                        <SwipeView.LeftItems>\r\n                            <SwipeItems>\r\n                                <SwipeItem \r\n                                             Text=\"Favorite\"\r\n                                             IconImageSource=\"favorite.png\"\r\n                                             BackgroundColor=\"LightGreen\"/>\r\n                                <SwipeItem \r\n                                             Text=\"Delete\"\r\n                                             IconImageSource=\"delete.png\"\r\n                                             BackgroundColor=\"LightPink\"/>\r\n                            </SwipeItems>\r\n                        </SwipeView.LeftItems>\r\n                        <!-- Content -->\r\n                        <Grid   \r\n                                HeightRequest=\"60\"\r\n                                WidthRequest=\"300\"\r\n                                BackgroundColor=\"DimGray\">\r\n                            <Label \r\n                                     Text=\"Swipe right\"\r\n                                     HorizontalOptions=\"Center\"\r\n                                     VerticalOptions=\"Center\"/>\r\n                        </Grid>\r\n                    </SwipeView>";
     #endregion
 
     #region [Overrides]
