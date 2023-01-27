cc0a0ab15b8e92b513c98ff3fd610725bc01d689
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2023-01-27 14:50:16 +0700

    Implement Table View

--- src/Features/Gallery/Pages/BuiltIn/TableView/TableViewPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/TableView/TableViewPageViewModel.cs
@@ -13,6 +13,9 @@ public partial class TableViewPageViewModel : NavigationAwareBaseViewModel
     #region [Properties]
     [ObservableProperty]
     IControlInfo controlInformation;
+
+    [ObservableProperty]
+    string standardTableViewXamlCode = "<TableView Intent=\"Menu\">\r\n    <TableRoot>\r\n        <TableSection Title=\"Chapters\">\r\n            <TextCell Text=\"1. Introduction to .NET MAUI\"\r\n                      Detail=\"Learn about .NET MAUI and what it provides.\" />\r\n            <TextCell Text=\"2. Anatomy of an app\"\r\n                      Detail=\"Learn about the visual elements in .NET MAUI\" />\r\n            <TextCell Text=\"3. Text\"\r\n                      Detail=\"Learn about the .NET MAUI controls that display text.\" />\r\n            <TextCell Text=\"4. Dealing with sizes\"\r\n                      Detail=\"Learn how to size .NET MAUI controls on screen.\" />\r\n            <TextCell Text=\"5. XAML vs code\"\r\n                      Detail=\"Learn more about creating your UI in XAML.\" />\r\n        </TableSection>\r\n    </TableRoot>\r\n</TableView>";
     #endregion
 
     #region [Overrides]
