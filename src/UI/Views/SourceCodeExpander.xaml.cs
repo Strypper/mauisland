c757b1e7c0d19e77b9b775eca041e3b4c76bb455
unknown <ocgrb.strypperjason115@gmail.com>
2023-02-09 04:27:53 +0700

    SourceCodeExpander V2

--- src/UI/Views/SourceCodeExpander.xaml.cs
+++ src/UI/Views/SourceCodeExpander.xaml.cs
@@ -1,24 +1,38 @@
+using ColorCode;
+using System.ComponentModel;
+
 namespace MAUIsland;
 
-public partial class SourceCodeExpander : ContentView
+public partial class SourceCodeExpander : ContentView, INotifyPropertyChanged
 {
     #region [CTor]
     public SourceCodeExpander()
     {
         InitializeComponent();
+    }
+    #endregion
 
+    #region [Fields]
+    private FormattedString outputFormattedString;
+    #endregion
+
+    #region [Properties]
+    public FormattedString OutputFormattedString
+    {
+        get { return outputFormattedString; }
+        set { outputFormattedString = value; OnPropertyChanged("OutputFormattedString"); }
     }
     #endregion
 
     #region [Bindable Properties]
-    public static readonly BindableProperty XamlCodeProperty = BindableProperty.Create( nameof(XamlCode),
+    public static readonly BindableProperty CodeProperty = BindableProperty.Create(nameof(Code),
                                                                                            typeof(string),
                                                                                            typeof(SourceCodeExpander),
                                                                                            default(string));
-    public string XamlCode
+    public string Code
     {
-        get => (string)GetValue(XamlCodeProperty);
-        set => SetValue(XamlCodeProperty, value);
+        get => (string)GetValue(CodeProperty);
+        set => SetValue(CodeProperty, value);
     }
 
 
@@ -33,10 +47,35 @@ public partial class SourceCodeExpander : ContentView
     }
     #endregion
 
+    #region [Event Handlers]
+
     private async void Copy_Clicked(object sender, EventArgs e) =>
-        await Clipboard.Default.SetTextAsync(XamlCode);
+        await Clipboard.Default.SetTextAsync(Code);
+    private void root_Loaded(object sender, EventArgs e)
+    {
+        CodeTypeLabel.Text = CodeType == CodeType.Xaml ? "Xaml Code" : "C# Code";
+        ApplyColor(Code, CodeType == CodeType.Xaml ? Languages.Xml : Languages.CSharp);
+    }
+
+    #endregion
+
+    #region [Methods]
+    private void ApplyColor(string code, ILanguage language)
+    {
+        var formatter = new FormattedStringFormatter();
+        var fs = new FormattedString();
+        formatter.FormatString(code, language, fs);
+
+        //Output = XamlServices.Save(fs);
+
+        OutputFormattedString = fs;
+    }
+    #endregion
+
 }
 
+
+#region [Converters]
 public class CodeTypeToStringConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -49,6 +88,7 @@ public class CodeTypeToStringConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+#endregion
 
 public partial class SourceCodeForSample : BaseModel
 {
