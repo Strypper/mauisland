2b9837a8adfcb567e4ef06eabd2a6fa2ad409653
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2023-02-09 11:55:40 +0700

    Initial Commit

---
+++ src/Features/Gallery/Pages/Syncfusion/SfComboBox/SfComboBoxPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUIsland.Features.Gallery.Pages.Syncfusion.SfComboBox;
+
+public partial class SfComboBoxPage : ContentPage
+{
+	public SfComboBoxPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Features/Gallery/Pages/Syncfusion/SfComboBox/SfComboBoxPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIsland;
+
+public partial class SfComboBoxPageViewModel : NavigationAwareBaseViewModel
+{
+
+    public SfComboBoxPageViewModel(
+        IAppNavigator appNavigator
+    ) : base(appNavigator)
+    {
+    }
+}
