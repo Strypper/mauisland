f44f0536ba70b8875cb366da86c2afef09cc5cca
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2023-02-19 08:59:07 +0700

    Add SFAvatarView

--- src/Features/Gallery/Pages/Syncfusion/SfAvatarView/SfAvatarViewPageViewModel.cs
+++ src/Features/Gallery/Pages/Syncfusion/SfAvatarView/SfAvatarViewPageViewModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace MAUIsland;
 public partial class SfAvatarViewPageViewModel : NavigationAwareBaseViewModel
 {
@@ -12,15 +14,49 @@ public partial class SfAvatarViewPageViewModel : NavigationAwareBaseViewModel
     #region [Properties]
     [ObservableProperty]
     IControlInfo controlInformation;
+
+    [ObservableProperty]
+    ObservableCollection<Employee> collectionImages;
+
     #endregion
 
     #region [Overrides]
     protected override void OnInit(IDictionary<string, object> query)
     {
-        //base.OnInit(query);
+        base.OnInit(query);
 
-        //ControlInformation = query.GetData<IControlInfo>();
-
+        ControlInformation = query.GetData<IControlInfo>();
+        CollectionImages = new ObservableCollection<Employee>();
+        CollectionImages.Add(new Employee { Name = "Charlie", ImageSource = "https://i.imgur.com/UPKRRWz.jpg", Colors = Colors.Gray });
+        CollectionImages.Add(new Employee { Name = "John", ImageSource = "https://i.imgur.com/wYB5zD1.jpg", Colors = Colors.Bisque });
+        CollectionImages.Add(new Employee { Name = "Justin", ImageSource = "https://i.imgur.com/yi64vKP.jpg", Colors = Colors.LightCoral });
     }
     #endregion
+}
+public class Employee
+{
+
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private string imagesource;
+
+    public string ImageSource
+    {
+        get { return imagesource; }
+        set { imagesource = value; }
+    }
+
+    private Color colors;
+
+    public Color Colors
+    {
+        get { return colors; }
+        set { colors = value; }
+    }
+
 }
