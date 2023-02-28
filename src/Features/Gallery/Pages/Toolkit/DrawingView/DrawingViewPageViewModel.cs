5c468d88078fe15bdaae78c2bcc62b1c6558c858
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2023-03-01 01:27:01 +0700

    Add Tk DrawingView

--- src/Features/Gallery/Pages/Toolkit/DrawingView/DrawingViewPageViewModel.cs
+++ src/Features/Gallery/Pages/Toolkit/DrawingView/DrawingViewPageViewModel.cs
@@ -14,8 +14,19 @@ public partial class DrawingViewPageViewModel : NavigationAwareBaseViewModel
     #region [Properties]
     [ObservableProperty]
     IControlInfo controlInformation;
+    
     [ObservableProperty]
-    IDrawingLine myLines;
+    string basicDrawingView = "<toolkit:DrawingView\r\n            Lines=\"{Binding MyLines}\"\r\n            LineColor=\"Red\"\r\n            LineWidth=\"5\" />";
+
+    [ObservableProperty]
+    string basicDrawingViewCSharp = "using CommunityToolkit.Maui.Views;\r\n\r\nvar drawingView = new DrawingView\r\n{\r\n    Lines = new ObservableCollection<IDrawingLine>(),\r\n    LineColor = Colors.Red,\r\n    LineWidth = 5\r\n};";
+
+    [ObservableProperty]
+    string multiLineUsage = "<views:DrawingView\r\n            Lines=\"{Binding MyLines}\"\r\n            IsMultiLineModeEnabled=\"true\"\r\n            ShouldClearOnFinish=\"false\" />";
+
+    [ObservableProperty]
+    string multiLineUsageCSharp = "using CommunityToolkit.Maui.Views;\r\n\r\nvar gestureImage = new Image();\r\nvar drawingView = new DrawingView\r\n{\r\n    Lines = new ObservableCollection<IDrawingLine>(),\r\n    IsMultiLineModeEnabled = true,\r\n    ShouldClearOnFinish = false,\r\n};";
+
     #endregion
 
     #region [Overrides]
