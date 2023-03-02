e0db5d96b2474a7949829486b86cb25d5c00a905
Seledy <seledy219@outlook.com>
2023-03-02 14:34:04 +0700

    Implement Build-in Popup Page

---
+++ src/Features/Gallery/Pages/BuiltIn/Popup/PopupControlInfo.cs
@@ -0,0 +1,15 @@
+namespace MAUIsland.Gallery.BuiltIn;
+class PopupControlInfo : IControlInfo 
+{
+    public string ControlName => "Pop up";
+    public string ControlRoute => typeof(PopupPage).FullName;
+    public ImageSource ControlIcon => new FontImageSource()
+    {
+        FontFamily = FontNames.FluentSystemIconsRegular,
+        Glyph = FluentUIIcon.Ic_fluent_square_shadow_12_regular
+    };
+    public string ControlDetail => "Displaying an alert, asking a user to make a choice, or displaying a prompt is a common UI task. .NET Multi-platform App UI (.NET MAUI) has three methods on the Page class for interacting with the user via a pop-up: DisplayAlert, DisplayActionSheet, and DisplayPromptAsync. Pop-ups are rendered with native controls on each platform.";
+    public string GitHubUrl => $"https://github.com/Strypper/mauisland/tree/main/src/Features/Gallery/Pages/BuiltIn/{ControlName}";
+    public string DocumentUrl => $"https://learn.microsoft.com/en-us/dotnet/maui/user-interface/pop-ups?view=net-maui-7.0";
+    public string GroupName => ControlGroupInfo.BuiltInControls;
+}
---
+++ src/Features/Gallery/Pages/BuiltIn/Popup/PopupPageViewModel.cs
@@ -0,0 +1,35 @@
+namespace MAUIsland;
+public partial class PopupPageViewModel : NavigationAwareBaseViewModel
+{
+    #region [CTor]
+    public PopupPageViewModel(
+        IAppNavigator appNavigator
+    ) : base(appNavigator)
+    {
+    }
+    #endregion
+
+    #region [Properties]
+    [ObservableProperty]
+    IControlInfo controlInformation;
+
+    [ObservableProperty]
+    string standardPopupCSharpCode = "    private async void Button_Clicked(object sender, EventArgs e)\r\n    {\r\n        await DisplayAlert(\"Sample Popup Alert\", \"You have been alerted\", \"OK\");\r\n    }";
+    [ObservableProperty]
+    string captureResponsePopupCSharpCode = "    private async void Button_Clicked_1(object sender, EventArgs e)\r\n    {\r\n        bool answer = await DisplayAlert(\"Question?\", \"Would you like to play a game\", \"Yes\",\"No\");\r\n        await DisplayAlert(\"Answer\",$\"Answer is {( answer ? \"yes\" : \"no\")}\" , \"OK\");\r\n    }";
+    [ObservableProperty]
+    string actionSheetPopupCSharpCode = "    private async void Button_Clicked_2(object sender, EventArgs e)\r\n    {\r\n        string answer = await DisplayActionSheet(\"What game do you wanna play?\", \"Cancel\", null, \"LOL\", \"Halo\", \"Dota2\");\r\n        if (answer != \"Cancel\")\r\n        {\r\n            await DisplayAlert(\"Answer\", $\"{answer} is great!\", \"OK\");\r\n        }\r\n    }";
+    [ObservableProperty]
+    string promptPopupCSharpCode = "    private async void Button_Clicked_3(object sender, EventArgs e)\r\n    {\r\n        string answer = await DisplayPromptAsync(\"Hello\", \"What's your name?\",placeholder: \"Type your name\");\r\n        if (answer != null) \r\n        {\r\n            await DisplayAlert(\"Welcome\", $\"Hello, {answer}\",\"Cancel\");\r\n        }\r\n    }";
+    #endregion
+
+    #region [Overrides]
+    protected override void OnInit(IDictionary<string, object> query)
+    {
+        base.OnInit(query);
+
+        ControlInformation = query.GetData<IControlInfo>();
+
+    }
+    #endregion
+}
