58062cadc346559bd8d53722d2a8808a14e82749
Nguyễn Hoàng Phi Long <onithelord@gmail.com>
2023-04-06 23:49:03 +0700

    FixGridColorSlider

---
+++ src/Features/Gallery/Pages/BuiltIn/Grid/DoubleToIntConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIsland.Gallery.BuiltIn;
+
+public class DoubleToIntConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        double doubleValue = (double)value*100;
+        int intValue = (int)doubleValue;
+        return intValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
