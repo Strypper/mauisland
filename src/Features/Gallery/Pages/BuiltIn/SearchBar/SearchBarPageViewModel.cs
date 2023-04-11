42ba04525d1eb235d8e723bc6429e12a1eb19acf
Nguyễn Hoàng Phi Long <onithelord@gmail.com>
2023-04-12 02:25:57 +0700

    AddSSearchByViewModel(RelayCommand)

--- src/Features/Gallery/Pages/BuiltIn/SearchBar/SearchBarPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/SearchBar/SearchBarPageViewModel.cs
@@ -1,19 +1,26 @@
 ﻿namespace MAUIsland;
 
-
 public partial class SearchBarPageViewModel : NavigationAwareBaseViewModel
 {
+    #region [Services]
+    private readonly IControlsService mauiControlsService;
+    #endregion
+
     #region [CTor]
-    public SearchBarPageViewModel(IAppNavigator appNavigator)
+    public SearchBarPageViewModel(IAppNavigator appNavigator,
+                                  IControlsService mauiControlsService)
                                 : base(appNavigator)
     {
-
+        this.mauiControlsService = mauiControlsService;
     }
     #endregion
 
     #region [Properties]
     [ObservableProperty]
     IControlInfo controlInformation;
+
+    [ObservableProperty]
+    ObservableCollection<IControlInfo> controlGroupList;
 
     [ObservableProperty]
     string standardSearchBarXamlCode = "<SearchBar Placeholder=\"Search items...\" />";
@@ -29,9 +36,10 @@ public partial class SearchBarPageViewModel : NavigationAwareBaseViewModel
     protected override void OnInit(IDictionary<string, object> query)
     {
         base.OnInit(query);
-
+        ControlGroupList = new ObservableCollection<IControlInfo>();
         ControlInformation = query.GetData<IControlInfo>();
 
+        LoadDataAsync().FireAndForget();
     }
     #endregion
 
@@ -39,5 +47,34 @@ public partial class SearchBarPageViewModel : NavigationAwareBaseViewModel
     [RelayCommand]
     Task OpenUrlAsync(string url)
     => AppNavigator.OpenUrlAsync(url);
+
+
+    [RelayCommand]
+    private async Task SearchAsync(string query)
+    {
+        ControlGroupList.Clear();
+
+        var items = await mauiControlsService.GetControlsAsync(ControlInformation.GroupName);
+
+        foreach (var item in items.Where(x => x.ControlName.Equals(query)))
+        {
+            ControlGroupList.Add(item);
+        }
+    }
+    #endregion
+
+    #region [Data]
+    private async Task LoadDataAsync()
+    {
+        ControlGroupList.Clear();
+
+        var items = await mauiControlsService.GetControlsAsync(ControlInformation.GroupName);
+
+        foreach (var item in items)
+        {
+            ControlGroupList.Add(item);
+        }
+        return;
+    }
     #endregion
 }
