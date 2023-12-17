a8097a02c5a4abb988d2dd983fdf1df411b7c120
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2023-12-17 11:44:40 +0700

    EnhanceMediaElement

--- src/Features/Gallery/Pages/Toolkit/Controls/MediaElement/MediaElementPage.xaml.cs
+++ src/Features/Gallery/Pages/Toolkit/Controls/MediaElement/MediaElementPage.xaml.cs
@@ -1,25 +1,38 @@
-﻿using CommunityToolkit.Maui.Views;
+﻿using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 using System.Net;
 
 namespace MAUIsland;
 public partial class MediaElementPage : IGalleryPage
 {
     #region [ Fields ]
-
+    readonly ILogger MediaElementLogger;
     string videoUrl = "https://petaversestorageaccount.blob.core.windows.net/petaverse-petvideos/Breathing 😂";
     string localFilePath = Path.Combine(FileSystem.Current.AppDataDirectory, "video.mp4");
     #endregion
 
     #region [CTor]
-    public MediaElementPage(MediaElementPageViewModel vm)
+    public MediaElementPage(MediaElementPageViewModel vm, ILogger<MediaElementPage> mediaElementLogger)
     {
         InitializeComponent();
 
         BindingContext = vm;
+        this.MediaElementLogger = mediaElementLogger;
+        MediaElement.PropertyChanged += MediaElementPropertyChanged;
     }
     #endregion
 
     #region [ Event Handlers ]
+    void MediaElementPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == MediaElement.DurationProperty.PropertyName)
+        {
+            MediaElementLogger.LogInformation("Duration: {newDuration}", MediaElement.Duration);
+            //PositionSlider.Maximum = MediaElement.Duration.TotalSeconds;
+        }
+    }
 
     private void ContentPage_Loaded(object sender, EventArgs e)
     {
@@ -76,5 +89,84 @@ public partial class MediaElementPage : IGalleryPage
             CheckFileLabel.Text = $"No file exist !!!";
         }
     }
+
+    void OnMediaOpened(object? sender, EventArgs e) 
+        => MediaElementLogger.LogInformation("Media opened.");
+
+    void OnStateChanged(object? sender, MediaStateChangedEventArgs e) 
+        => MediaElementLogger.LogInformation("Media State Changed. Old State: {PreviousState}, New State: {NewState}", e.PreviousState, e.NewState);
+
+    void OnMediaFailed(object? sender, MediaFailedEventArgs e) 
+        => MediaElementLogger.LogInformation("Media failed. Error: {ErrorMessage}", e.ErrorMessage);
+
+    void OnMediaEnded(object? sender, EventArgs e) 
+        => MediaElementLogger.LogInformation("Media ended.");
+
+    void OnPositionChanged(object? sender, MediaPositionChangedEventArgs e)
+    {
+        MediaElementLogger.LogInformation("Position changed to {position}", e.Position);
+        //PositionSlider.Value = e.Position.TotalSeconds;
+    }
+
+    void OnSeekCompleted(object? sender, EventArgs e) 
+        => MediaElementLogger.LogInformation("Seek completed.");
+
+    void OnSpeedMinusClicked(object? sender, EventArgs e)
+    {
+        if (MediaElement.Speed >= 1)
+        {
+            MediaElement.Speed -= 1;
+        }
+    }
+    void OnSpeedPlusClicked(object? sender, EventArgs e)
+    {
+        if (MediaElement.Speed < 10)
+        {
+            MediaElement.Speed += 1;
+        }
+    }
+    void OnVolumeMinusClicked(object? sender, EventArgs e)
+    {
+        if (MediaElement.Volume >= 0)
+        {
+            if (MediaElement.Volume < .1)
+            {
+                MediaElement.Volume = 0;
+
+                return;
+            }
+
+            MediaElement.Volume -= .1;
+        }
+    }
+
+    void OnVolumePlusClicked(object? sender, EventArgs e)
+    {
+        if (MediaElement.Volume < 1)
+        {
+            if (MediaElement.Volume > .9)
+            {
+                MediaElement.Volume = 1;
+
+                return;
+            }
+
+            MediaElement.Volume += .1;
+        }
+    }
+
+    void OnPlayClicked(object? sender, EventArgs e)
+        => MediaElement.Play();
+
+
+    void OnPauseClicked(object? sender, EventArgs e)
+        => MediaElement.Pause();
+
+
+    void OnStopClicked(object? sender, EventArgs e)
+        => MediaElement.Stop();
+
+    void OnMuteClicked(object? sender, EventArgs e)
+        => MediaElement.ShouldMute = !MediaElement.ShouldMute;
     #endregion
 }
--- src/Features/Gallery/Pages/Toolkit/Controls/MediaElement/MediaElementPageViewModel.cs
+++ src/Features/Gallery/Pages/Toolkit/Controls/MediaElement/MediaElementPageViewModel.cs
@@ -1,7 +1,11 @@
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
+
 namespace MAUIsland;
 public partial class MediaElementPageViewModel : NavigationAwareBaseViewModel
 {
-    #region [CTor]
+    #region [ CTor ]
     public MediaElementPageViewModel(
         IAppNavigator appNavigator
     ) : base(appNavigator)
@@ -9,24 +13,122 @@ public partial class MediaElementPageViewModel : NavigationAwareBaseViewModel
     }
     #endregion
 
-    #region [Properties]
+    #region [ Properties ]
+    [ObservableProperty]
+    double volume;
+
+    [ObservableProperty]
+    MediaElementState currentState;
+
     [ObservableProperty]
     IGalleryCardInfo controlInformation;
 
     [ObservableProperty]
-    string xamlNamespace = "xmlns:toolkit=\"http://schemas.microsoft.com/dotnet/2022/maui/toolkit\"";
+    string setupDescription = 
+        "In order to use the toolkit in XAML the following xmlns needs to be added into your page or view:";
 
     [ObservableProperty]
-    string xamlSimpleMediaElement = "<Frame Style=\"{x:StaticResource DocumentContentFrameStyle}\">\r\n                <toolkit:MediaElement ShouldShowPlaybackControls=\"True\" Source=\"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4\" />\r\n </Frame>";
+    string xamlNamespace = 
+        "xmlns:toolkit=\"http://schemas.microsoft.com/dotnet/2022/maui/toolkit\"";
+
+    [ObservableProperty]
+    string fullNamepaceExampleBefore =
+        "<ContentPage\r\n" +
+        "    x:Class=\"MAUIsland.MediaElementPage\"\r\n" +
+        "    xmlns=\"http://schemas.microsoft.com/dotnet/2021/maui\"\r\n" +
+        "    xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\r\n" +
+        "</ContentPage>";
+
+    [ObservableProperty]
+    string fullNamepaceExampleAfter = 
+        "<ContentPage\r\n" +
+        "    x:Class=\"MAUIsland.MediaElementPage\"\r\n" +
+        "    xmlns=\"http://schemas.microsoft.com/dotnet/2021/maui\"\r\n" +
+        "    xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\r\n" +
+        "    xmlns:toolkit=\"http://schemas.microsoft.com/dotnet/2022/maui/toolkit\">\r\n" +
+        "</ContentPage>";
+
+    [ObservableProperty]
+    string iOSBypassing = "To bypass the Hardware Silent Switch on iOS, add the following lines of code to MauiProgram.cs. This ensures that MediaElement's playback audio will always be audible to the user regardless of their device's Hardware Silent Switch.";
+
+    [ObservableProperty]
+    string iOSBypassingSetup =
+        "public static class MauiProgram\r\n" +
+        "{\r\n" +
+        "    public static MauiApp CreateMauiApp()\r\n\t" +
+        "    {\r\n" +
+        "#if IOS\r\n" +
+        "        AVAudioSession.SharedInstance().SetActive(true);\r\n" +
+        "        AVAudioSession.SharedInstance().SetCategory(AVAudioSessionCategory.Playback);\r\n" +
+        "#endif\r\n" +
+        "    }\r\n" +
+        "}";
+
+    [ObservableProperty]
+    string xamlSimpleRemoteMediaElement =
+        "<toolkit:MediaElement MinimumHeightRequest=\"200\"\r\n" +
+        "                      houldShowPlaybackControls=\"True\"\r\n" +
+        "                      Source=\"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4\" />";
+
+
+    [ObservableProperty]
+    string xamlSimpleLocalMediaElement =
+        "<toolkit:MediaElement Source = \"embed://MyFile.mp4\"\r\n" +
+        "                      ShouldShowPlaybackControls=\"True\" />";
+    [ObservableProperty]
+    string mediaSourceStaticMethodsExample =
+        "// FromFile\r\n" +
+        "var mediaSource = MediaSource.FromFile(\"localfile.mp4\");\r\n" +
+        "mediaElement.Source = mediaSource;\r\n\r\n " +
+        "// FromUri\r\n " +
+        "var mediaSource = MediaSource.FromUri(new Uri(\"https://website.com/media.mp4\"));\r\n" +
+        "mediaElement.Source = mediaSource;\r\n\r\n" +
+        "// FromResource\r\n" +
+        "var mediaSource = MediaSource.FromResource(\"YourAppName.Resources.media.mp4\");\r\n" +
+        "mediaElement.Source = mediaSource;";
+
+    [ObservableProperty]
+    string mediaSourceExample =
+        "// UriMediaSource\r\n" +
+        "var uriMediaSource = new UriMediaSource { Uri = new Uri(\"https://website.com/media.mp4\") };\r\n" +
+        "mediaElement.Source = uriMediaSource;\r\n\r\n" +
+        "// FileMediaSource\r\n" +
+        "var fileMediaSource = new FileMediaSource { File = \"media.mp4\" };\r\n" +
+        "mediaElement.Source = fileMediaSource;\r\n\r\n" +
+        "// ResourceMediaSource\r\n" +
+        "var resourceMediaSource = new ResourceMediaSource { Resource = \"YourAppName.Resources.media.mp4\" };\r\n" +
+        "mediaElement.Source = resourceMediaSource;";
+
     #endregion
 
-    #region [Overrides]
+    #region [ Overrides ]
     protected override void OnInit(IDictionary<string, object> query)
     {
         base.OnInit(query);
 
         ControlInformation = query.GetData<IGalleryCardInfo>();
-
     }
     #endregion
+
+    #region [ Relay Commands ]
+    [RelayCommand]
+    Task OpenUrlAsync(string url)
+    => AppNavigator.OpenUrlAsync(url);
+    #endregion
+
+    #region [ Load Data ]
+    //public Task LoadData()
+    //{
+    //    var stream = new StreamMediaSource;
+    //    var streamMediaSource = new StreamMediaSource { Stream = stream };
+    //    mediaElement.Source = streamMediaSource;
+    //}
+    #endregion
+}
+
+public partial class SupportedFormatsTable
+{
+    public string Platform { get; set; }
+    public string Link { get; set; }
+    public string Notes { get; set; }
 }
