a4c05a817d9e059ca899e8200c336893cb5b2e6d
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-01-10 05:24:34 +0700

    AddMemeService and FixStepperConverterName

---
+++ src/Features/Common/Services/Implementations/MrIncreadibleMemeService.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace MAUIsland;
+
+public class MrIncreadibleMemeService : IMrIncreadibleMemeService
+{
+    private IDictionary<double, ImageSource> images = new Dictionary<double, ImageSource>()
+    {
+        { 12, ImageSource.FromFile("mrincredibleblackwhite.png")},
+        { 13, ImageSource.FromFile("mrincredibleblackwhite1.png")},
+        { 14, ImageSource.FromFile("mrincredibleblackwhite2.png")},
+        { 15, ImageSource.FromFile("mrincredibleblackwhite3.png")},
+        { 16, ImageSource.FromFile("mrincredibleblackwhite4.png")},
+        { 17, ImageSource.FromFile("mrincredibleblackwhite5.png")},
+        { 18, ImageSource.FromFile("mrincrediblesmile.png")},
+        { 19, ImageSource.FromFile("mrincrediblesmile1.png")},
+        { 20, ImageSource.FromFile("mrincrediblesmile2.png")},
+        { 21, ImageSource.FromFile("mrincrediblesmile3.png")},
+        { 22, ImageSource.FromFile("mrincrediblesmile4.png")},
+        { 23, ImageSource.FromFile("mrincrediblesmile5.png")},
+    };
+
+    private IDictionary<double, string> titles = new Dictionary<double, string>()
+    {
+        { 12, "We send your IP Address to the FBI 🚓"},
+        { 13, "No stop !!! 🚫"},
+        { 14, "Bruh no !!! 🛑"},
+        { 15, "You kidding ? 🤡"},
+        { 16, "Really ? 💀"},
+        { 17, "Ok stop right there ✋🏽"},
+        { 18, "Lucky !!! 🍀"},
+        { 19, "Cool 😎"},
+        { 20, "Giggity ! 😏"},
+        { 21, "SOLID"},
+        { 22, "FINE"},
+        { 23, "MEH"},
+    };
+
+    public IDictionary<double, ImageSource> GetAllMemeImage()
+    {
+        return images;
+    }
+
+    public IDictionary<double, string> GetAllMemeTitle()
+    {
+        return titles;
+    }
+
+    public ImageSource GetMemeImage(double age)
+    {
+        return !images.ContainsKey(age) ? ImageSource.FromFile("mrincredibleblackwhite.png") : images[age];
+    }
+
+    public string GetMemeTitle(double age)
+    {
+        return !titles.ContainsKey(age) ? "Well crap !! 💀" : titles[age];
+    }
+}
--- src/Features/Gallery/Pages/BuiltIn/Controls/Stepper/StepperPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/Controls/Stepper/StepperPageViewModel.cs
@@ -15,10 +15,48 @@ public partial class StepperPageViewModel : NavigationAwareBaseViewModel
     IGalleryCardInfo controlInformation;
 
     [ObservableProperty]
-    string standardStepperXamlCode = "<StackLayout Margin=\"20\">\r\n                        <Label\r\n                            x:Name=\"_rotatingLabel\"\r\n                            FontSize=\"18\"\r\n                            HorizontalOptions=\"Center\"\r\n                            Text=\"ROTATING TEXT\"\r\n                            VerticalOptions=\"Center\" />\r\n                        <Stepper\r\n                            HorizontalOptions=\"Center\"\r\n                            Increment=\"30\"\r\n                            Maximum=\"360\"\r\n                            ValueChanged=\"OnStepperValueChanged\" />\r\n                        <Label\r\n                            x:Name=\"_displayLabel\"\r\n                            HorizontalOptions=\"Center\"\r\n                            Text=\"(uninitialized)\"\r\n                            VerticalOptions=\"Center\" />\r\n                    </StackLayout>";
+    string xamlStandardStepper = 
+        "<StackLayout Margin=\"20\">\r\n" +
+        "   <Label x:Name=\"RotatingLabel\"\r\n" +
+        "          Text=\"ROTATING TEXT\"\r\n" +
+        "          FontSize=\"18\"\r\n" +
+        "          VerticalOptions=\"Center\"\r\n" +
+        "          HorizontalOptions=\"Center\" />\r\n" +
+        "   <Stepper HorizontalOptions=\"Center\"\r\n" +
+        "            Increment=\"30\"\r\n" +
+        "            Maximum=\"360\"\r\n" +
+        "            ValueChanged=\"OnStepperValueChanged\" />\r\n" +
+        "   <Label x:Name=\"DisplayLabel\"\r\n" +
+        "          Text=\"(uninitialized)\"\r\n" +
+        "          HorizontalOptions=\"Center\"\r\n" +
+        "          VerticalOptions=\"Center\" />\r\n" +
+        "</StackLayout>";
 
     [ObservableProperty]
-    string dataBindAStepperXamlCode = "<StackLayout Margin=\"20\">\r\n                        <Label\r\n                            FontSize=\"18\"\r\n                            HorizontalOptions=\"Center\"\r\n                            Rotation=\"{Binding Source={x:Reference _stepper}, Path=Value}\"\r\n                            Text=\"ROTATING TEXT\"\r\n                            VerticalOptions=\"Center\" />\r\n                        <Stepper\r\n                            x:Name=\"_stepper\"\r\n                            HorizontalOptions=\"Center\"\r\n                            Increment=\"30\"\r\n                            Maximum=\"360\" />\r\n                        <Label\r\n                            HorizontalOptions=\"Center\"\r\n                            Text=\"{Binding Source={x:Reference _stepper}, Path=Value, StringFormat='The Stepper value is {0:F0}'}\"\r\n                            VerticalOptions=\"Center\" />\r\n                    </StackLayout>";
+    string csharpStandardStepper =
+        "private void OnStepperValueChanged(object sender, ValueChangedEventArgs e) \r\n" +
+        "{\r\n" +
+        "    double value = e.NewValue;\r\n" +
+        "    RotatingLabel.Rotation = value;\r\n" +
+        "    DisplayLabel.Text = string.Format(\"The Stepper value is {0}\", value);\r\n" +
+        "}";
+
+    [ObservableProperty]
+    string xamlDataBindAStepper =
+        "<StackLayout Margin=\"20\">\r\n" +
+        "   <Label Rotation=\"{Binding Source={x:Reference MyStepper}, Path=Value}\"\r\n" +
+        "          Text=\"ROTATING TEXT\"\r\n" +
+        "          FontSize=\"18\"\r\n" +
+        "          HorizontalOptions=\"Center\"\r\n" +
+        "          VerticalOptions=\"Center\" />\r\n" +
+        "   <Stepper x:Name=\"MyStepper\"\r\n" +
+        "            HorizontalOptions=\"Center\"\r\n" +
+        "            Increment=\"30\"\r\n" +
+        "            Maximum=\"360\" />\r\n" +
+        "   <Label Text=\"{Binding Source={x:Reference MyStepper}, Path=Value, StringFormat='The Stepper value is {0:F0}'}\"\r\n" +
+        "          HorizontalOptions=\"Center\"\r\n" +
+        "          VerticalOptions=\"Center\" />\r\n" +
+        "</StackLayout>";
 
     #endregion
 
