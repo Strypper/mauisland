13ebf0e018a79671078ad3673f60bdd06bf7d39c
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-01-12 00:38:28 +0700

    Add BoolToObjectConverter Document

--- src/Features/Gallery/Pages/Toolkit/Converters/BoolConverter/BoolToObjectConverterPageViewModel.cs
+++ src/Features/Gallery/Pages/Toolkit/Converters/BoolConverter/BoolToObjectConverterPageViewModel.cs
@@ -20,6 +20,31 @@ public partial class BoolToObjectConverterPageViewModel : NavigationAwareBaseVie
     bool converterTesting2 = false;
 
     [ObservableProperty]
+    string setupDescription =
+    "In order to use the toolkit in XAML the following xmlns needs to be added into your page or view:";
+
+    [ObservableProperty]
+    string xamlNamespace =
+        "xmlns:toolkit=\"http://schemas.microsoft.com/dotnet/2022/maui/toolkit\"";
+
+    [ObservableProperty]
+    string fullNamepaceExampleBefore =
+        "<ContentPage\r\n" +
+        "    x:Class=\"MAUIsland.MediaElementPage\"\r\n" +
+        "    xmlns=\"http://schemas.microsoft.com/dotnet/2021/maui\"\r\n" +
+        "    xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\r\n" +
+        "</ContentPage>";
+
+    [ObservableProperty]
+    string fullNamepaceExampleAfter =
+        "<ContentPage\r\n" +
+        "    x:Class=\"MAUIsland.MediaElementPage\"\r\n" +
+        "    xmlns=\"http://schemas.microsoft.com/dotnet/2021/maui\"\r\n" +
+        "    xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\r\n" +
+        "    xmlns:toolkit=\"http://schemas.microsoft.com/dotnet/2022/maui/toolkit\">\r\n" +
+        "</ContentPage>";
+
+    [ObservableProperty]
     string xamlConverterTesting =
         "<Label>\r\n" +
         "   <Label.FormattedText>\r\n" +
