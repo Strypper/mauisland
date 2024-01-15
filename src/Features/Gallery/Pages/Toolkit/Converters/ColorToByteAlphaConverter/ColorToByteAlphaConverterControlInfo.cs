5538f10ee307bf43bae810f6c344a2eca5c34a7a
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-01-15 20:12:37 +0700

    Fix Link

--- src/Features/Gallery/Pages/Toolkit/Converters/ColorToByteAlphaConverter/ColorToByteAlphaConverterControlInfo.cs
+++ src/Features/Gallery/Pages/Toolkit/Converters/ColorToByteAlphaConverter/ColorToByteAlphaConverterControlInfo.cs
@@ -13,7 +13,7 @@ class ColorToByteAlphaConverterControlInfo : ICommunityToolkitGalleryCardInfo
     };
     public string ControlDetail => "The ColorToByteAlphaConverter is a one way converter that allows users to convert an incoming Color to the alpha component as a value between 0 and 255.";
     public string GitHubUrl => $"https://github.com/Strypper/mauisland/tree/main/src/Features/Gallery/Pages/Toolkit/{ControlName}";
-    public string DocumentUrl => $"https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/converters/color-to-black-or-white-converter";
+    public string DocumentUrl => $"https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/converters/color-to-byte-alpha-converter";
     public string GroupName => ControlGroupInfo.CommunityToolkit;
     public GalleryCardType CardType => GalleryCardType.Converter;
     public GalleryCardStatus CardStatus => throw new NotImplementedException();
