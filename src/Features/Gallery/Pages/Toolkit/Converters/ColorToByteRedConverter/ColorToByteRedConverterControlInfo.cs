ac9d0021285db92f8acf2eb47590c6c14fe9386e
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-01-16 16:19:08 +0700

    ColorToByteRedConverterPage

--- src/Features/Gallery/Pages/Toolkit/Converters/ColorToByteRedConverter/ColorToByteRedConverterControlInfo.cs
+++ src/Features/Gallery/Pages/Toolkit/Converters/ColorToByteRedConverter/ColorToByteRedConverterControlInfo.cs
@@ -1,19 +1,19 @@
 ﻿namespace MAUIsland;
 
-class ColorToByteGreenConverterControlInfo : ICommunityToolkitGalleryCardInfo
+class ColorToByteRedConverterControlInfo : ICommunityToolkitGalleryCardInfo
 {
-    public string ControlName => "Color To Byte Green Converter";
+    public string ControlName => "Color To Byte Red Converter";
 
-    public string ControlRoute => typeof(ColorToByteGreenConverterPage).FullName;
+    public string ControlRoute => typeof(ColorToByteRedConverterPage).FullName;
     public ImageSource ControlIcon => new FontImageSource()
     {
         FontFamily = FontNames.FluentSystemIconsRegular,
         Size = 100,
         Glyph = FluentUIIcon.Ic_fluent_approvals_app_20_regular
     };
-    public string ControlDetail => "The ColorToByteGreenConverter is a one way converter that allows users to convert an incoming Color to the green component as a value between 0 and 255.";
+    public string ControlDetail => "The ColorToByteRedConverter is a one way converter that allows users to convert an incoming Color to the red component as a value between 0 and 255.";
     public string GitHubUrl => $"https://github.com/Strypper/mauisland/tree/main/src/Features/Gallery/Pages/Toolkit/{ControlName}";
-    public string DocumentUrl => $"https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/converters/color-to-byte-green-converter";
+    public string DocumentUrl => $"https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/converters/color-to-byte-red-converter";
     public string GroupName => ControlGroupInfo.CommunityToolkit;
     public GalleryCardType CardType => GalleryCardType.Converter;
     public GalleryCardStatus CardStatus => throw new NotImplementedException();
