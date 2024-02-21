961a41976efd982938a1aa86879cf571f3543c01
Xerath <abcxyz01233210@gmail.com>
2024-02-21 23:19:43 +0700

    Add localdb services for repo sync

---
+++ src/Features/Common/Models/LocalDb/RepositoryModel.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+
+namespace MAUIsland;
+
+[Table("RepositoryModel")]
+public class RepositoryModel : BaseLocalEntity
+{
+    #region [ CTor ]
+    public RepositoryModel() {
+
+    }
+    #endregion
+
+    #region [ Properties ]
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("full_name")]
+    public string FullName { get; set; }
+
+    [Column("svn_url")]
+    public string SvnUrl { get; set; }
+
+    [Column("owner_url")]
+    public string OwnerUrl { get; set; }
+
+    [Column("owner_avatar_url")]
+    public string OwnerAvatarUrl { get; set; }
+
+    [Column("forks_count")]
+    public int ForksCount { get; set; }
+
+    [Column("stargazers_count")]
+    public int StargazersCount { get; set; }
+
+    [Column("open_issues_count")]
+    public int OpenIssuesCount { get; set; }
+
+    [Column("license_name")]
+    public string LicenseName { get; set; }
+
+    [Column("updated_at")]
+    public DateTimeOffset UpdatedAt { get; set; }
+    #endregion
+}
---
+++ src/Features/Common/Services/Interfaces/LocalDb/IRepositorySyncService.cs
@@ -0,0 +1,10 @@
+﻿using Octokit;
+
+namespace MAUIsland;
+
+public interface IRepositorySyncService : ILocalDbService<RepositoryModel>
+{
+    #region [ Public Methods - Get Single ]
+    Task<Repository> GetRepositoryAsync(string ownerUrl, string repoName, string headerValue = "");
+    #endregion
+}
