56dcb4b4aabf875068290b0f14453b7e3c9ab754
Strypper Vandel Jason <ocgrb.strypperjason115@gmail.com>
2024-03-03 21:28:39 +0700

    Add border page

---
+++ src/Features/Gallery/Pages/BuiltIn/Controls/Border/BorderPage.xaml.cs
@@ -0,0 +1,54 @@
+using Microsoft.Maui.Controls.Shapes;
+
+namespace MAUIsland;
+
+public partial class BorderPage : IGalleryPage
+{
+
+    #region [ CTor ]
+
+
+    public BorderPage(BorderPageViewModel vm)
+    {
+        InitializeComponent();
+
+        BindingContext = vm;
+    }
+    #endregion
+
+    #region [ Event Handlers ]
+
+    private void BasePage_Loaded(object sender, EventArgs e)
+    {
+        CreateBorderUsingCSharp();
+    }
+    #endregion
+
+    #region [ Methods ]
+
+    void CreateBorderUsingCSharp()
+    {
+        Border border = new Border
+        {
+            Stroke = Color.FromArgb("#C49B33"),
+            Background = Color.FromArgb("#2B0B98"),
+            StrokeThickness = 4,
+            Padding = new Thickness(16, 8),
+            HorizontalOptions = LayoutOptions.Center,
+            StrokeShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(40, 0, 0, 40)
+            },
+            Content = new Label
+            {
+                Text = ".NET MAUI",
+                TextColor = Colors.White,
+                FontSize = 18,
+                FontAttributes = FontAttributes.Bold
+            }
+        };
+
+        CreateBorderExampleGrid.Add(border);
+    }
+    #endregion
+}
