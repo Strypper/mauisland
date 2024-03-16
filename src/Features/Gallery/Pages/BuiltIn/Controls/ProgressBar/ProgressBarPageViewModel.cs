644e846e6c15e31188a41ddbbaf693b1862ea1d0
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-03-16 18:53:05 +0700

    Code ReFactor

--- src/Features/Gallery/Pages/BuiltIn/Controls/ProgressBar/ProgressBarPageViewModel.cs
+++ src/Features/Gallery/Pages/BuiltIn/Controls/ProgressBar/ProgressBarPageViewModel.cs
@@ -15,9 +15,96 @@ public partial class ProgressBarPageViewModel : NavigationAwareBaseViewModel
     IGalleryCardInfo controlInformation;
 
     [ObservableProperty]
-    string standardProgressBarXamlCode = "<ProgressBar Progress=\"0\"\r\n             Margin=\"10\"\r\n             ProgressColor=\"Yellow\"/>";
+    string xamlStandardProgressBar =
+        "<ProgressBar Progress=\"0\"/>";
+
     [ObservableProperty]
-    string animateProgressBarXamlCode = "Control\r\n\r\n<ProgressBar x:Name=\"progress_bar2\"\r\n             Progress=\"0\"\r\n             Margin=\"10\"\r\n             ProgressColor=\"Red\"/>\r\n             \r\nClick Event\r\n\r\nprivate async void button2_Clicked(object sender, EventArgs e)\r\n    {\r\n        progress_bar2.Progress = 0;\r\n        button2.IsEnabled= false;\r\n        await progress_bar2.ProgressTo(0.99, 2000, Easing.BounceIn);\r\n        button2.IsEnabled= true;\r\n    }";
+    string xamlColorProgressBar =
+        "<Grid ColumnDefinitions=\"0.1*, 0.6*, 0.1*, 0.2*\">\r\n" +
+        "   <Label Grid.Column=\"0\"\r\n" +
+        "          Text=\"0%\" \r\n" +
+        "          VerticalOptions=\"Center\" \r\n" +
+        "          HorizontalOptions=\"Center\"/>\r\n" +
+        "   <ProgressBar x:Name=\"ProgressBar1\"\r\n" +
+        "                Grid.Column=\"1\"\r\n" +
+        "                Progress=\"0\"/>\r\n" +
+        "   <Label x:Name=\"ProgressLabel\" \r\n" +
+        "          Grid.Column=\"2\"\r\n" +
+        "          Text=\"0%\" \r\n" +
+        "          VerticalOptions=\"Center\" \r\n" +
+        "          HorizontalOptions=\"Center\"/>\r\n" +
+        "   <Button x:Name=\"ProgressBarLoadButton\"\r\n" +
+        "           Grid.Column=\"3\"\r\n" +
+        "           Clicked=\"ProgressBarLoadButtonClicked\"\r\n" +
+        "           Text=\"Load\"/>\r\n" +
+        "</Grid>";
+
+    [ObservableProperty]
+    string cSharpCcolorProgressBarCodeBehind =
+        "private async void ProgressBarLoadButtonClicked(object sender, EventArgs e)\r\n" +
+        "{\r\n" +
+        "    Progress = 0;\r\n\r\n" +
+        "    while (Progress < 1)\r\n" +
+        "    {\r\n" +
+        "        if(Progress == 0)\r\n" +
+        "        {\r\n" +
+        "            ProgressBar1.ProgressColor = Colors.Red;\r\n" +
+        "        }\r\n" +
+        "        Progress += 0.01;\r\n" +
+        "        await Task.Delay(1);\r\n" +
+        "        var progressIntValue = Progress * 100;\r\n" +
+        "        ProgressLabel.Text = $\"{progressIntValue:N1}%\";\r\n" +
+        "        switch ((int)progressIntValue)\r\n" +
+        "        {\r\n" +
+        "            case 30:\r\n" +
+        "                ProgressBar1.ProgressColor = Colors.OrangeRed;\r\n" +
+        "                break;\r\n" +
+        "            case 50:\r\n" +
+        "                ProgressBar1.ProgressColor = Colors.Orange;\r\n" +
+        "                break;\r\n" +
+        "            case 80:\r\n" +
+        "                ProgressBar1.ProgressColor = Colors.Yellow;\r\n" +
+        "                break;\r\n" +
+        "            case 90:\r\n" +
+        "                ProgressBar1.ProgressColor = Colors.YellowGreen;\r\n" +
+        "                break;\r\n" +
+        "            case 100:\r\n" +
+        "                ProgressBar1.ProgressColor = Colors.Green;\r\n" +
+        "                break;\r\n" +
+        "        }\r\n" +
+        "        ProgressBar1.Progress = Progress;\r\n" +
+        "    }\r\n" +
+        "}";
+
+    [ObservableProperty]
+    string xamlAnimateProgressBar = 
+        "";
+
+    [ObservableProperty]
+    string cSharpAnimateProgressBarCodeBehind =
+        "private async void ProgressBarRunButtonClicked(object sender, EventArgs e)\r\n" +
+        "{\r\n" +
+        "    ProgressBar2.Progress = 0;\r\n" +
+        "    ProgressBarRunButton.IsEnabled= false;\r\n" +
+        "    await ProgressBar2.ProgressTo(0.999, 5000, Easing.BounceIn);\r\n" +
+        "    ProgressBarRunButton.IsEnabled= true;\r\n" +
+        "}";
+
+    [ObservableProperty]
+    string cSharpAnimateProgressBarConverter =
+        "public class ProgressBarPercentageConverter : IValueConverter\r\n" +
+        "{\r\n" +
+        "    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)\r\n" +
+        "    {\r\n" +
+        "        double temp = (double)value * 100;\r\n" +
+        "        return $\"{temp:N1}%\";\r\n" +
+        "    }\r\n\r\n" +
+        "    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)\r\n" +
+        "    {\r\n" +
+        "        double temp = (double)value / 100;\r\n" +
+        "        return $\"{temp:N1}%\";\r\n" +
+        "    }\r\n" +
+        "}";
 
     #endregion
 
---
+++ src/Features/Gallery/Pages/BuiltIn/Controls/ProgressBar/ProgressBarPercentageConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIsland;
+
+public class ProgressBarPercentageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        double temp = (double)value * 100;
+        return $"{temp:N1}%";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        double temp = (double)value / 100;
+        return $"{temp:N1}%";
+    }
+}
