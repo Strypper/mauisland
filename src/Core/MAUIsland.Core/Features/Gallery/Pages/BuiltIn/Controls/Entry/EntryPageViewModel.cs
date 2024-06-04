a5d74c25e3aa824ddd0ebf886d7f88c709d93fc1
Strypper Vandel Jason <ocgrb.strypperjason115@gmail.com>
2024-06-04 12:44:53 +0700

    Enhance Entry page documentation: 1. Add 'Set the cursor position and text selection length' section. 2. Add 'Display a clear button' section. 3. Add 'Transform text' section. 4. Add 'Obscure text entry' section. 5. Add 'Customize the keyboard' section. 6. Add 'Customize the return key' section. 7. Add 'Hide and show the soft input keyboard' section. 8. Add 'Enable and disable spell checking' section. 9. Add 'Enable and disable text prediction' section. 10. Add 'Prevent text entry' section.

--- src/Core/MAUIsland.Core/Features/Gallery/Pages/BuiltIn/Controls/Entry/EntryPageViewModel.cs
+++ src/Core/MAUIsland.Core/Features/Gallery/Pages/BuiltIn/Controls/Entry/EntryPageViewModel.cs
@@ -179,6 +179,219 @@ public partial class EntryPageViewModel : BaseBuiltInPageControlViewModel
 
     [ObservableProperty]
     string setTheCursorPositionAndTextSelectionLength3 = "The default value of the SelectionLength property is 0, which indicates that no text is selected.";
+
+    [ObservableProperty]
+    string displayAClearButtonHeader = "Display a clear button";
+
+    [ObservableProperty]
+    string displayAClearButton = "The ClearButtonVisibility property can be used to control whether an Entry displays a clear button, which enables the user to clear the text. This property should be set to a ClearButtonVisibility enumeration member:";
+
+    [ObservableProperty]
+    List<string> displayAClearButtonList = new()
+    {
+        "Never indicates that a clear button will never be displayed. This is the default value for the ClearButtonVisibility property.",
+        "WhileEditing indicates that a clear button will be displayed in the Entry, while it has focus and text."
+    };
+
+    [ObservableProperty]
+    string displayAClearButtonXamlCode =
+    "<Entry Text=\".NET MAUI\"\n" +
+    "       ClearButtonVisibility=\"WhileEditing\" />";
+
+    [ObservableProperty]
+    string displayAClearButton1 = "The following example shows setting the property:";
+
+    [ObservableProperty]
+    string transformTextHeader = "Transform text";
+
+    [ObservableProperty]
+    string transformText = "An Entry can transform the casing of its text, stored in the Text property, by setting the TextTransform property to a value of the TextTransform enumeration. This enumeration has four values:";
+
+    [ObservableProperty]
+    List<string> transformTextList = new()
+    {
+        "None indicates that the text won't be transformed.",
+        "Default indicates that the default behavior for the platform will be used. This is the default value of the TextTransform property.",
+        "Lowercase indicates that the text will be transformed to lowercase.",
+        "Uppercase indicates that the text will be transformed to uppercase."
+    };
+
+    [ObservableProperty]
+    string transformText1 = "The following example shows transforming text to uppercase:";
+
+    [ObservableProperty]
+    string transformTextXamlCode =
+        "<Entry Text=\"This text will be displayed in uppercase.\"\n" +
+        "       TextTransform=\"Uppercase\" />";
+
+    [ObservableProperty]
+    string obscureTextEntryHeader = "Obscure text entry";
+
+    [ObservableProperty]
+    string obscureTextEntry = "Entry provides the IsPassword property which visually obscures entered text when it's set to true:";
+
+    [ObservableProperty]
+    string obscureTextEntryXamlCode = "<Entry IsPassword=\"true\" />";
+
+    [ObservableProperty]
+    string customizeTheKeyboardHeader = "Customize the keyboard";
+
+    [ObservableProperty]
+    string customizeTheKeyboard = "The soft input keyboard that's presented when users interact with an Entry can be set programmatically via the Keyboard property, to one of the following properties from the Keyboard class:";
+
+    [ObservableProperty]
+    List<string> customizeTheKeyboardList = new()
+    {
+        "Chat – used for texting and places where emoji are useful.",
+        "Default – the default keyboard.",
+        "Email – used when entering email addresses.",
+        "Numeric – used when entering numbers.",
+        "Plain – used when entering text, without any KeyboardFlags specified.",
+        "Telephone – used when entering telephone numbers.",
+        "Text – used when entering text.",
+        "Url – used for entering file paths & web addresses."
+    };
+
+    [ObservableProperty]
+    string customizeTheKeyboard1 = "The following example shows setting the Keyboard property:";
+
+    [ObservableProperty]
+    string customizeTheKeyboardXamlCode = "<Entry Keyboard=\"Chat\" />";
+
+    [ObservableProperty]
+    string customizeTheKeyboard2 = "The Keyboard class also has a Create factory method that can be used to customize a keyboard by specifying capitalization, spellcheck, and suggestion behavior. KeyboardFlags enumeration values are specified as arguments to the method, with a customized Keyboard being returned. The KeyboardFlags enumeration contains the following values:";
+
+    [ObservableProperty]
+    List<string> customizeTheKeyboardList1 = new()
+    {
+        "None – no features are added to the keyboard.",
+        "CapitalizeSentence – indicates that the first letter of the first word of each entered sentence will be automatically capitalized.",
+        "Spellcheck – indicates that spellcheck will be performed on entered text.",
+        "Suggestions – indicates that word completions will be offered on entered text.",
+        "CapitalizeWord – indicates that the first letter of each word will be automatically capitalized.",
+        "CapitalizeCharacter – indicates that every character will be automatically capitalized.",
+        "CapitalizeNone – indicates that no automatic capitalization will occur.",
+        "All – indicates that spellcheck, word completions, and sentence capitalization will occur on entered text."
+    };
+
+    [ObservableProperty]
+    string customizeTheKeyboard3 = "The following XAML code example shows how to customize the default Keyboard to offer word completions and capitalize every entered character:";
+
+    [ObservableProperty]
+    string customizeTheKeyboardXamlCode1 =
+    "<Entry Placeholder=\"Enter text here\">\n" +
+    "    <Entry.Keyboard>\n" +
+    "        <Keyboard x:FactoryMethod=\"Create\">\n" +
+    "            <x:Arguments>\n" +
+    "                <KeyboardFlags>Suggestions,CapitalizeCharacter</KeyboardFlags>\n" +
+    "            </x:Arguments>\n" +
+    "        </Keyboard>\n" +
+    "    </Entry.Keyboard>\n" +
+    "</Entry>";
+
+    [ObservableProperty]
+    string customizeTheKeyboard4 = "The equivalent C# code is:";
+
+    [ObservableProperty]
+    string customizeTheKeyboardCSharpCode =
+    "Entry entry = new Entry { Placeholder = \"Enter text here\" };\n" +
+    "entry.Keyboard = Keyboard.Create(KeyboardFlags.Suggestions | KeyboardFlags.CapitalizeCharacter);";
+
+    [ObservableProperty]
+    string customizeTheReturnKeyHeader = "Customize the return key";
+
+    [ObservableProperty]
+    string customizeTheReturnKey = "The appearance of the return key on the soft input keyboard, which is displayed when an Entry has focus, can be customized by setting the ReturnType property to a value of the ReturnType enumeration:";
+
+    [ObservableProperty]
+    List<string> customizeTheReturnKeyList = new()
+    {
+        "Default – indicates that no specific return key is required and that the platform default will be used.",
+        "Done – indicates a \"Done\" return key.",
+        "Go – indicates a \"Go\" return key.",
+        "Next – indicates a \"Next\" return key.",
+        "Search – indicates a \"Search\" return key.",
+        "Send – indicates a \"Send\" return key."
+    };
+
+    [ObservableProperty]
+    string customizeTheReturnKeyXamlCode = "<Entry ReturnType=\"Send\" />";
+
+    [ObservableProperty]
+    string customizeTheReturnKey1 = "The following XAML example shows how to set the return key:";
+
+    [ObservableProperty]
+    string customizeTheReturnKeyNote = "The exact appearance of the return key is dependent upon the platform. On iOS, the return key is a text-based button. However, on Android and Windows, the return key is a icon-based button.";
+
+    [ObservableProperty]
+    string customizeTheReturnKey2 = "When the Return key is pressed, the Completed event fires and any ICommand specified by the ReturnCommand property is executed. In addition, any object specified by the ReturnCommandParameter property will be passed to the ICommand as a parameter. For more information about commands, see Commanding.";
+
+    [ObservableProperty]
+    string hideAndShowTheSoftInputKeyboardHeader = "Hide and show the soft input keyboard";
+
+    [ObservableProperty]
+    string hideAndShowTheSoftInputKeyboard = "The SoftInputExtensions class, in the Microsoft.Maui namespace, provides a series of extension methods that support interacting with the soft input keyboard on controls that support text input. The class defines the following methods:";
+
+    [ObservableProperty]
+    List<string> hideAndShowTheSoftInputKeyboardList = new()
+    {
+        "IsSoftInputShowing, which checks to see if the device is currently showing the soft input keyboard.",
+        "HideSoftInputAsync, which will attempt to hide the soft input keyboard if it's currently showing.",
+        "ShowSoftInputAsync, which will attempt to show the soft input keyboard if it's currently hidden."
+    };
+
+    [ObservableProperty]
+    string hideAndShowTheSoftInputKeyboard1 = "The following example shows how to hide the soft input keyboard on an Entry named entry, if it's currently showing:";
+
+    [ObservableProperty]
+    string hideAndShowTheSoftInputKeyboardCSharpCode =
+    "if (entry.IsSoftInputShowing())\n" +
+    "    await entry.HideSoftInputAsync(System.Threading.CancellationToken.None);";
+
+    [ObservableProperty]
+    string enableAndDisableSpellCheckingHeader = "Enable and disable spell checking";
+
+    [ObservableProperty]
+    string enableAndDisableSpellChecking = "The IsSpellCheckEnabled property controls whether spell checking is enabled. By default, the property is set to true. As the user enters text, misspellings are indicated.";
+
+    [ObservableProperty]
+    string enableAndDisableSpellChecking1 = "However, for some text entry scenarios, such as entering a username, spell checking provides a negative experience and should be disabled by setting the IsSpellCheckEnabled property to false:";
+
+    [ObservableProperty]
+    string enableAndDisableSpellCheckingXamlCode = "<Entry ... IsSpellCheckEnabled=\"false\" />";
+
+    [ObservableProperty]
+    string enableAndDisableSpellCheckingNote = "When the IsSpellCheckEnabled property is set to false, and a custom keyboard isn't being used, the native spell checker will be disabled. However, if a Keyboard has been set that disables spell checking, such as Keyboard.Chat, the IsSpellCheckEnabled property is ignored. Therefore, the property cannot be used to enable spell checking for a Keyboard that explicitly disables it.";
+
+    [ObservableProperty]
+    string enableAndDisableTextPredictionHeader = "Enable and disable text prediction";
+
+    [ObservableProperty]
+    string enableAndDisableTextPrediction = "The IsTextPredictionEnabled property controls whether text prediction and automatic text correction is enabled. By default, the property is set to true. As the user enters text, word predictions are presented.";
+
+    [ObservableProperty]
+    string enableAndDisableTextPrediction1 = "However, for some text entry scenarios, such as entering a username, text prediction and automatic text correction provides a negative experience and should be disabled by setting the IsTextPredictionEnabled property to false:";
+
+    [ObservableProperty]
+    string enableAndDisableTextPredictionXamlCode = "<Entry ... IsTextPredictionEnabled=\"false\" />";
+
+    [ObservableProperty]
+    string enableAndDisableTextPredictionNote = "When the IsTextPredictionEnabled property is set to false, and a custom keyboard isn't being used, text prediction and automatic text correction is disabled. However, if a Keyboard has been set that disables text prediction, the IsTextPredictionEnabled property is ignored. Therefore, the property cannot be used to enable text prediction for a Keyboard that explicitly disables it.";
+
+    [ObservableProperty]
+    string preventTextEntryHeader = "Prevent text entry";
+
+    [ObservableProperty]
+    string preventTextEntry = "Users can be prevented from modifying the text in an Entry by setting the IsReadOnly property to true:";
+
+    [ObservableProperty]
+    string preventTextEntryXamlCode =
+    "<Entry Text=\"User input won't be accepted.\"\n" +
+    "       IsReadOnly=\"true\" />";
+
+
+    [ObservableProperty]
+    string preventTextEntryNote = "The IsReadonly property does not alter the visual appearance of an Entry, unlike the IsEnabled property that also changes the visual appearance of the Entry to gray.";
     #endregion
 
     #region [ Overrides ]
